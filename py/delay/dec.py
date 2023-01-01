2937b9da292654b96284dae7d03230dba391a44b
l-jinlin <l-jinlin@qianxin.com>
2023-01-01 17:43:34 +0800

    not999

---
+++ py/delay/dec.py
@@ -0,0 +1,41 @@
+
+
+def main(e):
+    # var Permissions = map[int]map[string]bool{
+    #     1:  PlatformInfoAPI,
+    #     4:  PlatformAccountAPI,
+    #     16: UniformAccountAPI,
+    # }
+
+    # for p= 1 ;p < permission; p = p << 2 :
+    #     if Permissions[permission & p][api] {
+    #         return true
+    #     }
+    # }
+    p = 1
+    fruits = ['apple', 'banana', 'cherry']
+    for x in fruits:
+        print(x)
+
+    fruits = ['apple', 'banana', 'cherry']
+    for x in fruits:
+        print(x)
+        if x == 'banana':
+            break
+
+        for x in range(2, 6):
+            print(x)
+        else:
+            print('Finally finished!')
+
+        a = 0
+        # a not in [9, 1]
+    # for p in range(16):
+    #     p = p << 2
+
+
+pass
+
+
+if __name__ == "__main__":
+    main('999999')
